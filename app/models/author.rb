6769bf1a889387e5e0abf7bf585850ea5cb8862f
Eric Loudon <loudon.eric@gmail.com>
2019-10-26 13:28:43 +0000

    Automatically backed up by Learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,4 @@
 class Author < ActiveRecord::Base
-  validates :name, presence: true
-  validates :name, uniquenss: true
+  validates :name, presence: true, uniqueness: true
   validates :phone_number, length: {maximum: 10}
 end
