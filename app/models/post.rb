6ab5d7a056bbc469ffd64465d292bcd80d6ef310
Eric Loudon <loudon.eric@gmail.com>
2019-10-26 13:29:43 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,13 @@
 class Post < ActiveRecord::Base
   include ActiveModel::Validations
-validates :title, presence: true
-validates :content, length: { minimum: 10 }
-validates :summary, length: { in: 6..20 }
-validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "%{value} is not a valid category" }
-validate :clickbait
+  validates :title, presence: true
+  validates :content, length: { minimum: 10 }
+  validates :summary, length: { in: 6..20 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "%{value} is not a valid category" }
+  validate :clickbait
 
 private
+
 def clickbait
   clickbait_arr = [/Won't Believe/i, /Secret/i, /Top [0-9]*/i, /Guess/i]
   if clickbait_arr.none? do |i|
@@ -14,5 +15,5 @@ def clickbait
   end
     self.errors[:title] << "You need to be more clickbait-y"
   end
-end
+ end
 end
